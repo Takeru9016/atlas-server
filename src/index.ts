e4f98ff700927a5120bcded476b14491bd81518a
Takeru9016 <youthfox046@gmail.com>
2024-11-09 23:14:02 +0530

    feat: Set up Express server with project routes & Prisma integration

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+
+import projectRoutes from "./routes/projectRoutes";
+
+/* CONFIGURATION */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+/* ROUTES */
+app.get("/", (req, res) => {
+  res.send("This is Home Route");
+});
+
+app.use("/projects", projectRoutes);
+
+/* SERVER */
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
+});
